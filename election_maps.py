3e45c077db5a935a2e775ee9c913203dbebd529b
Joel Bradshaw <cincodenada@gmail.com>
2016-10-20 21:56:31 -0700

    Refactor wiki interface into a class
    Mostly so I can stub things out for testing.
    
    Also, let's play with yield, whee!

--- election_maps.py
+++ election_maps.py
@@ -4,35 +4,63 @@ import yaml
 meta = yaml.load(open('election_meta.yaml','r'))
 
 wikipedia.set_lang('commons')
-print("Getting list of maps...")
-map_page = wikipedia.page('Template:US_presidential_election_maps_SVG')
-# Not sure why I get all here, but whatevs
-map_list = map_page.nslinks('File')
-map_list = [l for l in map_list if l.startswith('File:')]
+
+class MapGetter:
+    def __init__(self, fake):
+        self.fake = fake
+
+    def get_maps(self):
+        print("Getting list of maps...")
+        if(self.fake):
+            yield "File:ElectoralCollege2016.svg"
+            return
+
+        map_page = wikipedia.page('Template:US_presidential_election_maps_SVG')
+        # Not sure why I get all here, but whatevs
+        for l in map_page.nslinks('File'):
+            if l.startswith('File:'):
+                yield l
+
+    def maps(self):
+        for m in self.get_maps():
+            if(self.fake):
+                yield {
+                    'file': m,
+                    'thumb': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/ElectoralCollege2016.svg/349px-ElectoralCollege2016.svg.png',
+                }
+            else:
+                print("Getting {}...".format(curmap))
+                curmap_page = wikipedia.page(curmap)
+                print("Getting thumbnail")
+                thumbs = curmap_page.query({
+                    'prop': 'imageinfo',
+                    'iiprop': 'url',
+                    'iiurlwidth': base['thumbwidth'],
+                })
+                thumb = next(thumbs)
+                yield {
+                    'file': m,
+                    'thumb': thumb['thumburl']
+                }
+
+
+mg = MapGetter(True)
+map_list = mg.get_maps()
 
 outfile = open('test.html','w')
-curmap = map_list.pop()
-base = meta['bases']['full']
+for curmap in mg.maps():
+    base = meta['bases']['full']
+    scale = base['thumbwidth']/base['width']
 
-print("Getting {}...".format(curmap))
-curmap_page = wikipedia.page(curmap)
-print("Getting thumbnail")
-thumbs = curmap_page.query({
-    'prop': 'imageinfo',
-    'iiprop': 'url',
-    'iiurlwidth': base['thumbwidth'],
-})
-thumb = next(thumbs)
-
-scale = base['thumbwidth']/base['width']
-
-outfile.write('<map id="testmap" name="testmap">\n')
-for (state, area) in meta['areas']['full'].items():
-    outfile.write('<area href="//en.wikipedia.org/wiki/United_States_presidential_election_in_{},_2016" shape="{}" coords="{}">\n'.format(
-        state,
-        area['shape'],
-        ','.join([str(round(int(p)*scale)) for p in area['points'].split(' ')])
+    outfile.write('<map id="{0}" name="{0}">\n'.format(curmap['file']))
+    for (state, area) in meta['areas']['full'].items():
+       outfile.write('<area href="//en.wikipedia.org/wiki/United_States_presidential_election_in_{},_2016" shape="{}" coords="{}">\n'.format(
+           state,
+           area['shape'],
+           ','.join([str(round(int(p)*scale)) for p in area['points'].split(' ')])
+    outfile.write('</map>\n')
+    outfile.write('<img src="{}" usemap="#{}" width="349" height="203" />\n'.format(
+        curmap['thumb'],
+        curmap['file']
     ))
-outfile.write('</map>\n')
-outfile.write('<img src="{}" usemap="#testmap" width="349" height="203" />\n'.format(thumb['thumburl']))
-outfile.close()
+    outfile.close()
