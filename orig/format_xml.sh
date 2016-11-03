f6e58ffb6075dacd1d435526be073245c0dbd798
Joel Bradshaw <cincodenada@gmail.com>
2016-11-02 22:40:39 -0700

    Add utility to make downloads pretty

---
+++ orig/format_xml.sh
@@ -0,0 +1,10 @@
+for i in `ls *.html`; do
+    if [ `wc -l $i | cut -d\  -f1` -eq 1 ]; then
+        tidy -xml -w 0 $i > tmp
+#       MAPSTART=`grep -n -m1 map | cut -d: -f1`
+#       tail -n+$MAPSTART > tmp_tail
+#       MAPEND=`grep -n -m1 img`
+#       head -n$MAPEND > $i
+        mv tmp $i
+    fi
+done
