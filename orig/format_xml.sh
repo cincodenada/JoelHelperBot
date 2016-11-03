f97a7358115e7a4771cb7a01612dba612e6ca2e0
Joel Bradshaw <cincodenada@gmail.com>
2016-11-02 23:07:00 -0700

    Quiet formatter

--- orig/format_xml.sh
+++ orig/format_xml.sh
@@ -1,6 +1,6 @@
 for i in `ls *.html`; do
     if [ `wc -l $i | cut -d\  -f1` -eq 1 ]; then
-        tidy -xml -w 0 $i > tmp
+        tidy -xml -q -w 0 $i > tmp
 #       MAPSTART=`grep -n -m1 map | cut -d: -f1`
 #       tail -n+$MAPSTART > tmp_tail
 #       MAPEND=`grep -n -m1 img`
